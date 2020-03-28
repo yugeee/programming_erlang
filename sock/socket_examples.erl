8c5c68f717000e3227a37fcf7a724f8bba183c97
弓削直樹 <sscyuge@gmail.com>
2020-03-28 22:44:05 +0900

    TCPサーバ作成

--- sock/socket_examples.erl
+++ sock/socket_examples.erl
@@ -1,5 +1,5 @@
 -module(socket_examples).
--export([nano_get_url/0, nano_get_url/1]).
+-export([nano_get_url/0, nano_get_url/1, start_nano_server/0, nano_client_eval/1, start_seq_server/0, start_parallel_server/0]).
 
 nano_get_url() ->
     nano_get_url('www.google.com').
@@ -16,3 +16,66 @@ receive_data(Socket, SoFar) ->
         {tcp_closed, Socket} ->
             list_to_binary(lists:reverse(SoFar))
     end.
+
+%  一回だけリクエストをうけるTCPサーバ
+start_nano_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    {ok, Socket} = gen_tcp:accept(Listen),
+    gen_tcp:close(Listen),
+    loop(Socket).
+
+% リクエスト作成
+loop(Socket) ->
+    receive
+        {tcp, Socket, Bin} ->
+            io:format("Server received binary = ~p~n", [Bin]),
+            Str = binary_to_term(Bin),
+            io:format("Server (unpacked)  ~p~n", [Str]),
+            Reply = lib_misc:string2value(Str),
+            io:format("Server replying = ~p~n", [Reply]),
+            gen_tcp:send(Socket, term_to_binary(Reply)),
+            loop(Socket);
+        {tcp_closed, Socket} ->
+            io:format("Server socket closed~n")
+    end.
+
+%  一回だけリクエストをうけるTCPサーバ
+nano_client_eval(Str) -> 
+    {ok, Socket} =
+        gen_tcp:connect("localhost", 2345,
+                        [binary, {packet, 4}]),
+    ok = gen_tcp:send(Socket, term_to_binary(Str)),
+    receive
+        {tcp, Socket, Bin} -> 
+            io:format("Client received binary = ~p~n", [Bin]),
+            Val = binary_to_term(Bin),
+            io:format("Client result = ~p~n", [Val]),
+            gen_tcp:close(Socket)
+    end.
+
+%  何度も実行可能なTCPサーバ　
+start_seq_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    seq_loop(Listen).
+
+seq_loop(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    loop(Socket),
+    seq_loop(Listen).
+
+
+% 複数のリクエストを受けられるサーバ
+start_parallel_server() ->
+    {ok, Listen} = gen_tcp:listen(2345, [binary, {packet, 4},
+                                         {reuseaddr, true},
+                                         {active, true}]),
+    spawn(fun() -> par_connect(Listen) end).
+
+par_connect(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    spawn(fun() -> par_connect(Listen) end),
+    loop(Socket).
